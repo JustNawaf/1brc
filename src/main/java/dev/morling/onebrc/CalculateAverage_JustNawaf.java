5c1544eaa98635d9bceddb055344cdd6def5d6c9
Nawaf Alharbi <nawaf.k.alharbi@outlook.sa>
2025-02-27 00:28:56 +0300

    wip

--- src/main/java/dev/morling/onebrc/CalculateAverage_JustNawaf.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_JustNawaf.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -14,9 +17,14 @@ import java.util.stream.Collectors;
 
 public class CalculateAverage_JustNawaf {
     private static final String FILE = "./measurements.txt";
-    private static final int THREAD_COUNT = Runtime.getRuntime().availableProcessors();
+//    private static final int THREAD_COUNT = Runtime.getRuntime().availableProcessors();
+    private static final int THREAD_COUNT = 1;
 
 
+    // TODO: CREATE A CLASS TO COLLECT MEASUREMENTS AND CALCULATING IT.
+    // TODO: REWRITE THE CODE.
+    // TODO: IDEA: SPLIT THE FILES INTO CHUNKS AND PROCESS THEM IN PARALLEL THEN MERGE IT.
+    
     private static record Measurement(String station, double value) {
         public Measurement(String [] parts){
             this(parts[0], Double.parseDouble(parts[1]));
@@ -61,120 +69,99 @@ public class CalculateAverage_JustNawaf {
         return endPos;
     }
 
-    private static ArrayList<Measurement> measurements(FileChannel channel, long startPos, long endPos) throws IOException {
-        ArrayList<Measurement> measurements = new ArrayList<>();
-
-        MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, startPos, endPos);
-
-        StringBuilder line = new StringBuilder();
-
-        while (buffer.hasRemaining()) {
-            char c = (char) buffer.get();
-
-            if(c == '\n'){
-                measurements.add(new Measurement(line.toString().split(";")));
-                line = new StringBuilder();
-            }
-
-            line.append(c);
-        }
-
-        channel.close();
-        return measurements;
+    private static String getX(RandomAccessFile file) throws IOException {
+        return file.readLine();
     }
 
-    private static HashMap<Long, Long> getPositions(long startPos, long chunkSize) throws IOException {
-        HashMap<Long, Long> positions = new HashMap<>();
+    private static ArrayList<MappedByteBuffer> getBuffers() throws IOException {
+        ArrayList<MappedByteBuffer> buffers = new ArrayList<MappedByteBuffer>();
+        RandomAccessFile file = new RandomAccessFile(FILE, "r");
+        long chunkSize = file.length() / THREAD_COUNT;
+        long startPos = 0;
+        FileChannel channel = file.getChannel();
 
         for (int i = 0; i < THREAD_COUNT; i++) {
             long endPos = getEndPosition(startPos, chunkSize);
-            positions.put(startPos, endPos);
+            MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, startPos, (int) endPos - startPos);
+            buffers.add(buffer);
             startPos = endPos;
         }
 
-        return positions;
+        return buffers;
     }
 
-    private static ArrayList<Measurement> processMeasurements(FileChannel channel, Map<Long, Long> positions) throws InterruptedException, ExecutionException, FileNotFoundException {
-        RandomAccessFile file = new RandomAccessFile(FILE, "r");
-        ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
-        ArrayList<Future<ArrayList<Measurement>>> futures = new ArrayList<>();
+    private static ArrayList<ResultRow> processMeasurements(ArrayList<MappedByteBuffer> buffers) throws InterruptedException, ExecutionException {
+        ArrayList<ResultRow> results = new ArrayList<>();
 
-        for (Map.Entry<Long, Long> entry : positions.entrySet()) {
-            long startPos = entry.getKey();
-            long endPos = entry.getValue();
+        System.out.println(buffers);
 
-            // Submit each measurement processing task
-            Future<ArrayList<Measurement>> future = executor.submit(() -> {
-                try {
-                    return measurements(file.getChannel(), startPos, endPos);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+        List<ArrayList<MeasurementAggregator>> l = buffers.stream().map(buffer -> {
+            System.out.println("Starting measurements");
+            System.out.println(buffer.hasRemaining());
+
+            Map<String, Integer> index = new HashMap<>();
+            ArrayList<MeasurementAggregator> aggs = new ArrayList<>();
+
+            StringBuilder line = new StringBuilder();
+
+            while (buffer.hasRemaining()) {
+                char c = (char) buffer.get();
+
+                if(c == '\n'){
+                    Measurement mgr = new Measurement(line.toString().split(";"));
+
+                    if(index.containsKey(mgr.station())){
+                        MeasurementAggregator agg = aggs.get(index.get(mgr.station));
+                        agg.min = Math.min(agg.min, mgr.value());
+                        agg.max = Math.max(agg.max, mgr.value());
+                        agg.count++;
+                        agg.sum += mgr.value();
+                    } else {
+                        MeasurementAggregator agg  = new MeasurementAggregator();
+                        agg.min = mgr.value();
+                        agg.max = mgr.value();
+                        agg.count++;
+                        agg.sum += mgr.value();
+                        aggs.add(agg);
+
+                        index.put(mgr.station(), aggs.indexOf(agg));
+                    }
+
+                    line = new StringBuilder();
                 }
-            });
 
-            futures.add(future);
-        }
+                line.append(c);
+            }
 
-        // Merge all thread outputs into a single list
-        ArrayList<Measurement> mergedResults = new ArrayList<>();
-        for (Future<ArrayList<Measurement>> future : futures) {
-            mergedResults.addAll(future.get());
-        }
+            System.out.println("Total measurements: " + aggs.size());
+            return aggs;
+        }).toList();
 
-        executor.shutdown();
-        return mergedResults;
+//        for (ArrayList<MeasurementAggregator> aggs : l) {
+//            for(MeasurementAggregator agg : aggs){
+//                System.out.println(agg.max);
+//            }
+//        }
+        return results;
     }
 
     public static void main(String[] args) throws IOException {
         var startTime = System.currentTimeMillis();
 
         RandomAccessFile file = new RandomAccessFile(FILE, "r");
-        System.out.println(file.length());
-        FileChannel channel = file.getChannel();
 
-        long chunkSize = file.length() / THREAD_COUNT;
+        ArrayList<MappedByteBuffer> buffers = getBuffers();
 
-        HashMap<Long, Long> positions = getPositions(0, chunkSize);
+        try {
+            ArrayList<ResultRow> measurements = processMeasurements(buffers);
 
+//            Map<String, ResultRow> collect  = measurements.stream().collect(Collectors.groupingBy(Measurement::station, collector));
 
-        Collector<Measurement, MeasurementAggregator, ResultRow> collector = Collector.of(
-            MeasurementAggregator::new,
-          (a, m) -> {
-              a.min = Math.min(a.min, m.value);
-              a.max = Math.max(a.max, m.value);
-              a.sum += m.value;
-              a.count++;
-          },
+//            System.out.println(collect);
 
-            (agg1, agg2) -> {
-                var a = new MeasurementAggregator();
-                a.max = Math.max(agg1.max, agg2.max);
-                a.min = Math.min(agg1.min, agg2.min);
-                a.count = agg1.count + agg2.count;
-                a.sum = agg1.sum + agg2.sum;
-
-                return a;
-            },
-
-            (agg) -> {
-                return new ResultRow(agg.min, (Math.round(agg.sum * 10.0) / 10.0) / agg.count, agg.max);
-            }
-        );
-        System.out.println(positions);
-        positions.forEach((startPos, endPos) -> {
-
-            try {
-                ArrayList<Measurement> measurements = processMeasurements(channel, positions);
-
-                Map<String, ResultRow> collect  = measurements.stream().collect(Collectors.groupingBy(Measurement::station, collector));
-                System.out.println(collect);
-            } catch (InterruptedException | ExecutionException e) {
-                throw new RuntimeException(e);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
 //
 //
 
