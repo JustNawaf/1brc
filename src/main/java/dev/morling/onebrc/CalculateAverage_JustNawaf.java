f49b9c04c15b43b6db8a289d595b70ba41e3c525
Nawaf Alharbi <nawaf.k.alharbi@outlook.sa>
2025-03-02 04:39:38 +0300

    6 seconds

--- src/main/java/dev/morling/onebrc/CalculateAverage_JustNawaf.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_JustNawaf.java
@@ -1,6 +1,7 @@
 package dev.morling.onebrc;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.*;
@@ -8,15 +9,24 @@ import java.util.*;
 public class CalculateAverage_JustNawaf {
     private static final String FILE = "./measurements.txt";
     private static final int THREAD_COUNT = Runtime.getRuntime().availableProcessors();
-//    private static final int THREAD_COUNT = 1;
-    private static Map<String, Integer> index = new HashMap<>();
-    private static ArrayList<Result> results = new ArrayList<>();
+    public static final int CITY_NAME_MAX_CHARACTERS = 128;
 
 
-    private static record Measurement(String station, double value) {
-        public Measurement(String [] parts){
-            this(parts[0].trim(), Double.parseDouble(parts[1]));
+    private static record Measurement(String station, int value) {}
+
+    private static int readNumberFromBuffer(ByteBuffer buffer) {
+        var number = 0;
+        var sign = 1;
+        while (buffer.hasRemaining()) {
+            var numberByte = buffer.get();
+            if (numberByte == '-')
+                sign = -1;
+            else if (numberByte == '\n')
+                break;
+            else if (numberByte != '.')
+                number = number * 10 + (numberByte - '0');
         }
+        return sign * number;
     }
 
     private static class Result {
@@ -39,9 +49,48 @@ public class CalculateAverage_JustNawaf {
             this.average = Math.round((this.sum / this.count) * 10.0) / 10.0;
         }
 
-        public String toString() {
-            return STR."\{station}=\{min}/\{average}/\{max}";
+        public Result merge(Result partialResult) {
+            Result n = new Result(this.station);
+            n.min = Math.min(this.min, partialResult.min);
+            n.max = Math.max(this.max, partialResult.max);
+            n.sum += partialResult.sum;
+            n.count += partialResult.count;
+            n.average = Math.round((this.sum / this.count) * 10.0) / 10.0;
+
+            return n;
         }
+
+        public String toString() {
+            return station + "=%.1f/%.1f/%.1f".formatted(min / 10.0, sum / 10.0 / count, max / 10.0);
+        }
+    }
+
+    private static class ResultAgg {
+        Map<String, Result> results = new HashMap<>(10_000, 1);
+
+
+        public void add(Measurement measurement) {
+            if(results.containsKey(measurement.station())){
+                Result result = results.get(measurement.station());
+                result.addTemparture(measurement.value());
+            } else {
+                Result r = new Result(measurement.station());
+                r.addTemparture(measurement.value());
+                results.put(measurement.station(), r);
+            }
+        }
+
+        public void merge(ResultAgg other) {
+            other.results.forEach((station, result) -> results.merge(station, result, (existing, incoming) -> {
+                existing.min = Math.min(existing.min, incoming.min);
+                existing.max = Math.max(existing.max, incoming.max);
+                existing.sum += incoming.sum;
+                existing.count += incoming.count;
+                existing.average = Math.round((existing.sum / existing.count) * 10.0) / 10.0;
+                return existing;
+            }));
+        }
+
     }
 
     private static long getEndPosition(long startPos, long chunkSize) throws IOException {
@@ -90,42 +139,72 @@ public class CalculateAverage_JustNawaf {
 
         long startTime = System.currentTimeMillis();
 
-        buffers.parallelStream().forEach(buffer -> {
-            StringBuilder line = new StringBuilder();
+        ResultAgg values = buffers.parallelStream().map(buffer -> {
+            byte[] name = new byte[CITY_NAME_MAX_CHARACTERS];
+            int value = 0;
+            
+            boolean pass = false;
+
+            ResultAgg agg = new ResultAgg();
+            int lastIndexName = 0;
 
             while (buffer.hasRemaining()) {
+
                 try{
-                    char c = (char) buffer.get();
-                    if(c == '\n'){
-                        Measurement mgr = new Measurement(line.toString().split(";"));
-
-                        if(index.containsKey(mgr.station())){
-                            Result result = results.get(index.get(mgr.station()));
-                            result.addTemparture(mgr.value());
-                        } else {
-                            Result r = new Result(mgr.station());
-                            r.addTemparture(mgr.value());
-                            results.add(r);
-                            index.put(mgr.station(), results.size() - 1);
-                        }
-
-                        line = new StringBuilder();
+                    byte c = buffer.get();
+                    if(pass){
+                        agg.add(new Measurement(new String(name, 0, lastIndexName), value));
+                        lastIndexName = 0;
+                        pass = false;
                     }
 
-                    line.append(c);
+                    if((char) c == ';'){
+                        int sign = 1;
+                        int number = 0;
+
+                        while(buffer.hasRemaining()){
+                            byte b = buffer.get();
+
+                            if((char) b == '\n'){
+                                pass = true;
+                                break;
+                            }
+
+                            if((char) b == '-'){
+                                sign = -1;
+                                continue;
+                            }
+
+                            if((char) b != '.'){
+                                number = number * 10 + (b - '0');
+                            }
+                        }
+
+                        value = sign * number;
+
+                    } else {
+                        name[lastIndexName++] = c;
+                    }
                 }
                  catch (Exception e){
                     break;
                  }
             }
+
+            return agg;
+        }).reduce(new ResultAgg(), (agg1, agg2) -> {
+            ResultAgg result = new ResultAgg();
+            result.merge(agg1);
+            result.merge(agg2);
+            return result;
         });
 
         long endTime = System.currentTimeMillis();
-        for (Result r : results) {
+        for (Result r : values.results.values()) {
             System.out.println(r);
         }
 
         System.out.println((endTime - startTime) / 1000 + " seconds");
-        System.out.println("Total measurements: " + results.size());
+        System.out.println("Total measurements: " + values.results.size());
     }
 }
